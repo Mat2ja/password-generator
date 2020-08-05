c7379778c3080a2fb3e53f436cbad4eb3e6728bd
Matija Osrecki <matija.osrecki@gmail.com>
2020-08-05 11:32:47 +0200

    added more options and refactored code :sparkles:
    - added excludeSimilar and excludeAmbiguous checkoboxes
    - replaced function parameters with object and object destructuring

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const uppercaseEl = document.getElementById('uppercase');
 const lowercaseEl = document.getElementById('lowercase');
 const numbersEl = document.getElementById('numbers');
 const symbolsEl = document.getElementById('symbols');
+const similarEl = document.getElementById('similar');
+const ambiguousEl = document.getElementById('ambiguous');
 const generateEl = document.getElementById('generate');
 const clipboardEl = document.getElementById('clipboard');
 
@@ -12,7 +14,7 @@ const randomFunc = {
     lower: getRandomLower,
     upper: getRandomUpper,
     number: getRandomNumber,
-    symbol: getRandomSymbol,
+    symbol: getRandomSymbol
 }
 
 //! Generate event on generate button click
@@ -22,8 +24,18 @@ generateEl.addEventListener('click', () => {
     const hasUpper = uppercaseEl.checked;
     const hasNumber = numbersEl.checked;
     const hasSymbol = symbolsEl.checked;
+    const excludeSimilar = similarEl.checked;
+    const excludeAmbiguous = ambiguousEl.checked;
 
-    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+    resultEl.innerText = generatePassword({
+        lower: hasLower,
+        upper: hasUpper,
+        number: hasNumber,
+        symbol: hasSymbol,
+        excludeSimilar,
+        excludeAmbiguous,
+        length
+    });
 });
 
 // Copy password to clipboard
@@ -40,88 +52,110 @@ clipboardEl.addEventListener('click', () => {
     textarea.remove();
 
     // TODO handle prompt better
-    // let prompt = document.createElement('p');
-    // let hasPrompt = true;
-    // prompt.innerHTML = 'Password copied to clipboard';
-    // let container = document.querySelector('.container');
-
-    // container.insertAdjacentElement('afterend', prompt);
 
     alert('Password copied to clipboard');
 })
 
 // Generate password
-function generatePassword(lower, upper, number, symbol, length) {
-    // 1. Init password variable
-    // 2. Filter out unchecked types
-    // 3. Loop over length
-    // 4. Add final password to password variable and return it
+function generatePassword({ lower, upper, number, symbol, excludeSimilar, excludeAmbiguous, length }) {
 
     let generatedPassword = '';
 
     // counts 'true' values
     const typesCount = lower + upper + number + symbol;
-    // filter out false
+
+    // create array of objects, filter out 'false'
     const typesArr = [{ lower }, { upper }, { number }, { symbol }]
         .filter(item => Object.values(item)[0]);
 
+    // console.log(typesArr);
 
+    // if none is checked
     if (typesCount === 0) {
         return '';
     }
 
-    // for (let i = 0; i < length; i += typesCount) {
-    //     typesArr.forEach(type => {
-    //         const funcName = Object.keys(type)[0];
-    //         generatedPassword += randomFunc[funcName]();
-    //     });
-    // };
-    // console.log(generatedPassword.slice(0, length));
+    // reset length to max if value too large
     if (length > 20) {
         length = 20;
         lengthEl.value = length;
     }
-    //* my solution; more random
+    if (length < 6) {
+        length = 6;
+        lengthEl.value = 6;
+    }
+
     for (let i = 0; i < length; i++) {
         // choose random type from the checked types
         let type = typesArr[Math.floor(Math.random() * typesArr.length)];
+        // get the key of the checked value
         const funcName = Object.keys(type)[0];
 
-        generatedPassword += randomFunc[funcName]();
+        // get random charact by passing checked value as the key
+        let randomChar = randomFunc[funcName]({ excludeSimilar, excludeAmbiguous });
+
+        generatedPassword += randomChar;
     };
+
     console.clear();
-    console.log('%cGenerated password:', 'color:salmon', generatedPassword,);
+    console.log('%cGenerated password:', 'color:salmon', generatedPassword);
 
     return generatedPassword;
 }
 
 
 // Generator functions
-function getRandom(minChar, maxChar) {
-    let min = minChar.charCodeAt(0);
-    let max = maxChar.charCodeAt(0);
-    let rand = Math.floor(Math.random() * (max - min + 1) + min);
-    return String.fromCharCode(rand);
+function getRandom({ startChar, endChar, excludeSimilar }) {
+    let similarCharacters = ["i", "l", "1", "o", "0", "O"];
+
+    let start = startChar.charCodeAt(0);
+    let end = endChar.charCodeAt(0);
+
+    let randChar = String.fromCharCode(Math.floor(Math.random() * (end - start + 1) + start));
+
+    while (excludeSimilar && similarCharacters.includes(randChar)) {
+        randChar = String.fromCharCode(Math.floor(Math.random() * (end - start + 1) + start));
+    }
+
+    return randChar;
 }
 
-function getRandomLower() {
-    return getRandom('a', 'z');
+function getRandomLower({ excludeSimilar }) {
+    return getRandom({
+        startChar: 'a',
+        endChar: 'z',
+        excludeSimilar
+    });
 }
 
-function getRandomUpper() {
-    return getRandom('A', 'Z');
+function getRandomUpper({ excludeSimilar }) {
+    return getRandom({
+        startChar: 'A',
+        endChar: 'Z',
+        excludeSimilar
+    })
 }
 
-function getRandomNumber() {
-    return getRandom('0', '9')
+function getRandomNumber({ excludeSimilar }) {
+    return getRandom({
+        startChar: '0',
+        endChar: '9',
+        excludeSimilar
+    });
 }
 
-function getRandomSymbol() {
-    const symbols = '!"#$%&\'()*+,-./:;<=>?@[]^_`{}|~';
-    let rand = Math.floor(Math.random() * symbols.length);
-    return symbols[rand];
-}
+function getRandomSymbol({ excludeAmbiguous }) {
+    const symbols = ["!", "\"", "#", "$", "%", "&", "\\", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"];
+    const ambiguousCharacters = ["{", "}", "[", "]", "(", ")", "/", "\\", "'", "\"", "`", "~", ",", ";", ":", ".", "<", ">"];
 
+    let randSymbol = symbols[Math.floor(Math.random() * symbols.length)];
+
+    while (excludeAmbiguous && ambiguousCharacters.includes(randSymbol)) {
+        randSymbol = symbols[Math.floor(Math.random() * symbols.length)];
+    }
+
+    return randSymbol;
+}
 
 
 
